e212b624586753c5a96b365a6fb555b6d0217469
Anindya Das <dasanindya15@gmail.com>
2026-06-06 21:22:41 -0400

    Update solution: 155. Min Stack

--- 155. Min Stack/155. Min Stack.py
+++ 155. Min Stack/155. Min Stack.py
@@ -1,31 +1,50 @@
 #
 # Problem: 155. Min Stack
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/min-stack/submissions/2024810354/
+# Link: https://leetcode.com/problems/min-stack/
 # Language: python3
 # Date: 2026-06-07
 
 
 class MinStack:
+    # single stack
+    # def __init__(self):
+    #     # Each entry is (value, min_so_far_including_this_value)
+    #     self.stack = []
 
+    # def push(self, value: int) -> None:
+    #     current_min = value if not self.stack else min(value, self.stack[-1][1])
+    #     self.stack.append([value, current_min])
+
+    # def pop(self) -> None:
+    #     self.stack.pop()
+
+    # def top(self) -> int:
+    #     return self.stack[-1][0]
+
+    # def getMin(self) -> int:
+    #     return self.stack[-1][1]
+
+
+    # The two-stack variant
     def __init__(self):
-        # Each entry is (value, min_so_far_including_this_value)
         self.stack = []
+        self.mins = []   # parallel stack of running minimums
 
-    def push(self, value: int) -> None:
-        current_min = value if not self.stack else min(value, self.stack[-1][1])
-        self.stack.append([value, current_min])
+    def push(self, val) -> None:
+        self.stack.append(val)
+        if not self.mins or val <= self.mins[-1]: # <= is mandatory, Consider pushing [2, 1, 1]
+            self.mins.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
+        if self.stack.pop() == self.mins[-1]:
+            self.mins.pop()
 
     def top(self) -> int:
-        return self.stack[-1][0]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.stack[-1][1]
-
-
+        return self.mins[-1]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
