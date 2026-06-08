09b070a51b5a1132fdcc7f22e0e16eae0f2164c7
Anindya Das <dasanindya15@gmail.com>
2026-06-08 10:35:59 -0400

    Update solution: 226. Invert Binary Tree

--- 226. Invert Binary Tree/226. Invert Binary Tree.py
+++ 226. Invert Binary Tree/226. Invert Binary Tree.py
@@ -1,7 +1,7 @@
 #
 # Problem: 226. Invert Binary Tree
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/invert-binary-tree/
+# Link: https://leetcode.com/problems/invert-binary-tree/submissions/2026471544/
 # Language: python3
 # Date: 2026-06-08
 
@@ -19,4 +19,25 @@ class Solution:
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         return root
 
-        
+## What LeetCode does behind the scenes
+# from collections import deque
+
+# def build_tree(values):
+#     if not values:
+#         return None
+#     root = TreeNode(values[0])
+#     queue = deque([root])
+#     i = 1
+#     while queue and i < len(values):
+#         node = queue.popleft()
+#         # Left child
+#         if i < len(values) and values[i] is not None:
+#             node.left = TreeNode(values[i])
+#             queue.append(node.left)
+#         i += 1
+#         # Right child
+#         if i < len(values) and values[i] is not None:
+#             node.right = TreeNode(values[i])
+#             queue.append(node.right)
+#         i += 1
+#     return root
