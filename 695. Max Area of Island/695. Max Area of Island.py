410f3c611c0f9f5e9069a98d34a32207c6fc7fc5
Anindya Das <dasanindya15@gmail.com>
2026-06-12 13:09:43 -0400

    Update solution: 695. Max Area of Island

--- 695. Max Area of Island/695. Max Area of Island.py
+++ 695. Max Area of Island/695. Max Area of Island.py
@@ -1,7 +1,7 @@
 #
 # Problem: 695. Max Area of Island
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/max-area-of-island/submissions/2031037381/
+# Link: https://leetcode.com/problems/max-area-of-island/submissions/2031038713/
 # Language: python3
 # Date: 2026-06-12
 
@@ -13,30 +13,25 @@ class Solution:
 
         rows = len(grid)
         cols = len(grid[0])
-        areas = []
+        max_area = 0  
 
-        def get_area_using_dfs(i:int, j:int):
-            if i<0 or j<0 or i>=rows or j>=cols or grid[i][j] == 0:
+        def get_area_using_dfs(i: int, j: int) -> int:
+            if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] == 0:
                 return 0
             
-            grid[i][j] = 0
+            grid[i][j] = 0  
             
-            a1 = get_area_using_dfs(i-1,j)
-            a2 = get_area_using_dfs(i+1,j)
-            a3 = get_area_using_dfs(i,j-1)
-            a4 = get_area_using_dfs(i,j+1)
+            a1 = get_area_using_dfs(i-1, j)
+            a2 = get_area_using_dfs(i+1, j)
+            a3 = get_area_using_dfs(i, j-1)
+            a4 = get_area_using_dfs(i, j+1)
 
-            return a1+a2+a3+a4+1
-
+            return a1 + a2 + a3 + a4 + 1
 
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 1:
-                    area = get_area_using_dfs(i,j)
-                    areas.append(area)
-        if areas:
-            return max(areas)
-        else:
-            return 0
-                
-        
+                    current_area = get_area_using_dfs(i, j)
+                    max_area = max(max_area, current_area)
+                    
+        return max_area
