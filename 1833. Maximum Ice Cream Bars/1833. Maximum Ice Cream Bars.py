5d9d88c4d7ca527ad7833bcee3c41950f7225255
Anindya Das <dasanindya15@gmail.com>
2026-06-21 08:48:28 -0400

    Add solution: 1833. Maximum Ice Cream Bars

---
+++ 1833. Maximum Ice Cream Bars/1833. Maximum Ice Cream Bars.py
@@ -0,0 +1,11 @@
+#
+# Problem: 1833. Maximum Ice Cream Bars
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/maximum-ice-cream-bars/submissions/2040987681/?envType=daily-question&envId=2026-06-21
+# Language: python3
+# Date: 2026-06-21
+
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        
