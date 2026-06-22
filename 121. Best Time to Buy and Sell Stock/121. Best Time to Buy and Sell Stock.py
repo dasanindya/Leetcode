1886e50021cf53fc7d68385900f8133b589293bd
Anindya Das <dasanindya15@gmail.com>
2026-06-22 00:24:42 -0400

    Update solution: 121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.py
@@ -1,13 +1,14 @@
 #
 # Problem: 121. Best Time to Buy and Sell Stock
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/2024800035/
+# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/2041597310/
 # Language: python3
-# Date: 2026-06-07
+# Date: 2026-06-22
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # it remembers the lowest valley (min_price) we've crossed so far and continuously checks how high the current peak is relative to that valley.
         min_price = float("inf")
         max_profit = 0
         for price in prices:
