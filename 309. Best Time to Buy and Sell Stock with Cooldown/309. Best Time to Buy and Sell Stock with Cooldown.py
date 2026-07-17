09f68f98e5a374e17caea4d0fdd0243abfcb4268
Anindya Das <dasanindya15@gmail.com>
2026-07-17 07:41:20 -0400

    Update solution: 309. Best Time to Buy and Sell Stock with Cooldown

--- 309. Best Time to Buy and Sell Stock with Cooldown/309. Best Time to Buy and Sell Stock with Cooldown.py
+++ 309. Best Time to Buy and Sell Stock with Cooldown/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,7 +1,7 @@
 #
 # Problem: 309. Best Time to Buy and Sell Stock with Cooldown
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/submissions/2071042937/
+# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 # Language: python3
 # Date: 2026-07-17
 
@@ -9,6 +9,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # dp hashmap to store (index, buying_state) -> max_profit
+        # buying_state -> Buy/Sell
+        # If buy i -> i+1
+        # If sell i -> i+2 
         dp = {}  
         
         def dfs(i: int, buying: bool) -> int:
@@ -20,17 +23,18 @@ class Solution:
             if (i, buying) in dp:
                 return dp[(i, buying)]
             
-            # Decision 1: Do nothing (Cooldown/Rest) and move to the next day
-            cooldown = dfs(i + 1, buying)
-            
             if buying:
-                # Decision 2a: Buy today (subtract cost) and shift state to "not buying" for tomorrow
-                buy = dfs(i + 1, False) - prices[i]
+                # Decision a: Buy today (subtract cost) and shift state to "not buying" for tomorrow
+                buy = dfs(i + 1, not buying) - prices[i]
+                # Decision: Do nothing (Cooldown/Rest) today and move to the next day
+                cooldown = dfs(i + 1, buying)
                 # Cache the maximum profit between buying vs resting
                 dp[(i, buying)] = max(buy, cooldown)
             else:
                 # Decision 2b: Sell today (add gain) and shift state to "buying" after a 1-day cooldown (i + 2)
-                sell = dfs(i + 2, True) + prices[i]
+                sell = dfs(i + 2, not buying) + prices[i] # not buying -> True
+                # Decision: Do nothing (Cooldown/Rest) today and move to the next day
+                cooldown = dfs(i + 1, buying)
                 # Cache the maximum profit between selling vs resting
                 dp[(i, buying)] = max(sell, cooldown)
                 
